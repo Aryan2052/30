aa3068b72c302f879e432816042863114ffcfdab
Aryan2052 <aryandangat2005@gmail.com>
2025-06-28 22:44:28 +0530

    java

---
+++ solve/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();  // number of test cases
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 3 == 0)
+                System.out.println("Second");
+            else
+                System.out.println("First");
+        }
+
+        sc.close();
+    }
+}
